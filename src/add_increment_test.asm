e38b3b248fe89e80a7cb2f0b9a0b7c3628973c32
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-04-19 17:45:33 +0200

    Added comments to assembly file

---
+++ src/add_increment_test.asm
@@ -0,0 +1,6 @@
+ld a, #0    ;load 0 into register a
+ld b, #0    ;load 0 into register b
+inc b       ;increment register b by 1
+inc b       ;increment register b by 1
+add a, #10  ;add 10 to register a
+add a, b    ;add register b to register a
--- src/test.asm
+++
@@ -1,6 +0,0 @@
-ld a, #0
-ld b, #0
-inc b
-inc b
-add a, #10
-add a, b
