4def14fd84f20c0f567074e52a694459ee28faea
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-04-21 12:29:01 +0200

    Calling cpu in main for testing

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from utilities import Utilities
 from display import Display
+from cpu import Cpu
 
 
 if __name__ == "__main__":
     print(Utilities.read_asm_file("src/add_increment_test.asm"))
-    z80_display = Display()
-    z80_display.run()
+    CPU = Cpu()
+    print(CPU)
