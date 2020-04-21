b49f292e96b95f520959a7fddf5320c0aea6e29c
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-04-21 17:10:38 +0200

    Added beginning to lexer

--- lexer.py
+++ lexer.py
@@ -1,10 +1,30 @@
+from typing import List
+
+
 class Token(object):
-    def __init__(self, token_type, value):
+    def __init__(self, token_type, value, line):
         self.token_type = token_type
         self.value = value
+        self.line = line
 
     def __str__(self):
-        return f"Token({self.token_type}, {self.value})"
+        return f"Token(Type:{self.token_type}, Value:{self.value}, Line:{self.line})"
 
     def __repr__(self):
         return self.__str__()
+
+
+def tokenizer(item: str, line_number: int) -> Token:
+    # TODO: Tokenize stuff here
+    return Token(item, None, line_number)
+
+
+# lexer :: List[str] -> int -> List[Token]
+def lexer(code: List[str], line_num: int = 0) -> List[Token]:
+    if len(code) == 0:
+        return []
+    else:
+        head, *tail = code
+        result = list(map(lambda x: tokenizer(x, line_num), head.split()))
+        line_num += 1
+        return result + lexer(tail, line_num)
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from utilities import Utilities
 from display import Display
 from cpu import Cpu
+import lexer
 
 
 if __name__ == "__main__":
-    print(Utilities.read_asm_file("src/add_increment_test.asm"))
-    CPU = Cpu()
-    print(CPU)
+    code = Utilities.read_asm_file("src/add_increment_test.asm")
+    tokens = lexer.lexer(code)
+    for token in tokens:
+        print(token)
