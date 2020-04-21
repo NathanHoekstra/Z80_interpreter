97042b41b9540eddb52ac26f0cfabcd12d911d96
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-04-21 12:28:20 +0200

    Added cpu class for Z80

---
+++ cpu.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class Cpu(object):
+    def __init__(self):
+        # Z80's memory size is 64KB
+        self.memory = [np.uint8(0)] * 0xFFFF
+        # Registers
+        self.a = np.uint8(0x00)
+        self.b = np.uint8(0x00)
+        self.sp = np.uint16(0xFFFE)
+        self.pc = np.uint16(0x1000)
+
+    def __str__(self):
+        return f"Register A:        [{hex(self.a)}]\n" + \
+               f"Register B:        [{hex(self.b)}]\n" + \
+               f"Stack Pointer:     [{hex(self.sp)}]\n" + \
+               f"Program Counter:   [{hex(self.pc)}]\n"
+
+    def __repr__(self):
+        return self.__str__()
