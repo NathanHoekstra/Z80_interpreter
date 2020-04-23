4291b5b812d2039fedb4952372d78d501bc7e96b
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-04-23 19:10:09 +0200

    Added test with loop

--- src/add_increment_test.asm
+++ src/add_increment_test.asm
@@ -4,3 +4,7 @@ inc b       ;increment register b by 1
 inc b       ;increment register b by 1
 add a, #10  ;add 10 to register a
 add a, b    ;add register b to register a
+loop:
+    add a, #1 ;add 1 to register a
+    djnz loop ;loop if b is not zero
+ret ;exit program
