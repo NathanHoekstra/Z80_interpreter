93b72b2becba6d32e3fa430bdfc800d7228c0013
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-04-29 17:07:26 +0200

    Added extra printing to main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from helpers.utilities import Utilities
 import lexer
-import parser
+from token_parser import parser
 
 
 if __name__ == "__main__":
     code = Utilities.read_asm_file("src/add_increment_test.asm")
+    print("\n----- Stage 1 ----\n")
     tokens = lexer.lexer(code)
     for token in tokens:
         print(token)
+    print("\n----- Stage 2 ----\n")
     # Parse the token list
-    parsed = parser.parser(tokens)
-    print(parsed)
+    parsed = parser(tokens)
+    for parsed_item in parsed:
+        print(parsed_item)
