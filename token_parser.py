8dd7577cb3f90ddf8b249a3d49b57d821abca8c1
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-04-30 14:35:43 +0200

    Rewrote token_parser (now works as expected)

--- token_parser.py
+++ token_parser.py
@@ -1,51 +1,43 @@
-from typing import List
+from typing import List, Tuple, Union
 from lexer import Token
 from helpers.rules import parser_rules
-from helpers.enums import TokenType as tt
+from helpers.enums import TokenType
 
 
-def check_rules(token: Token, token_index: int, rules: List) -> bool:
+# validate_tokens :: Tuple[TokenType] -> List[Tuple] -> Bool
+def validate_tokens(tokens: Tuple[TokenType], rules: List[Tuple]) -> bool:
     # Out of rules to check, so no matching rule was found
     if len(rules) == 0:
         return False
-    head, *tail = rules
-    # If a matching rule was found
-    t_type = token.token_type
-    if t_type == tt.DECIMAL or t_type == tt.HEXADECIMAL or t_type == tt.BINARY:
-        t_type = tt.VALUE
-    try:
-        if head[token_index] == t_type:
-            return True
-    # Try the next item
-    except IndexError:
-        return check_rules(token, token_index, tail)
-    # otherwise look at the next item in the rule list
-    return check_rules(token, token_index, tail)
+    rule, *tail = rules
+    if tokens == rule:  # If a match has been found
+        return True
+    # No match has been found with the current rule, let's try the next rule
+    return validate_tokens(tokens, tail)
+
+
+# get_tokens :: List[Token] -> int -> List[Token] -> Union[List[Token], [List[TokenType]]
+def get_tokens(tokens: List[Token], curr_line: int, just_type: bool = False) -> Union[List[Token], List[TokenType]]:
+    # No more tokens to go through, let's exit out of the recursion
+    if len(tokens) == 0:
+        return []
+    head, *tail = tokens
+    if head.line == curr_line:  # Is the current token still on the correct line?
+        if just_type:  # Do we just want the token type?
+            head = head.token_type
+        return [head] + get_tokens(tail, curr_line, just_type)
+    return []
 
 
 # parser :: List[Token] -> int -> List[List[Token]]
-def parser(token_list: List[Token], i_index: int = 0, current_line: int = 0, new_list: List = None) -> List[List[Token]]:
-    if new_list is None:
-        new_list = []
+def parser(token_list: List[Token], current_line: int = 1) -> List[List[Token]]:
     # We went through the token list
     if len(token_list) == 0:
-        return new_list
-    head, *tail = token_list
-    # check if this is the first iteration
-    if current_line == 0:
-        current_line = head.line
-    # if the current line is not equal to the current token line, so next line
-    elif current_line != head.line:
-        # set the current line to the new line
-        current_line = head.line
-        # reset the item index
-        i_index = 0
-    if check_rules(head, i_index, parser_rules):
-        if i_index == 0:
-            new_list.append([head])
-        else:
-            new_list[current_line - 1].append(head)
-        i_index += 1
-        return parser(tail, i_index, current_line, new_list)
+        return []
+    curr_types = get_tokens(token_list, current_line, True)
+    curr_tokens = get_tokens(token_list, current_line)
+    if validate_tokens(tuple(curr_types), parser_rules):
+        current_line += 1
+        return [curr_tokens] + parser(token_list[len(curr_tokens):], current_line)
     else:
-        raise ValueError(f"The token: {head} on line: {current_line} is invalid")
+        raise ValueError(f"Invalid syntax on line {current_line}: {' '.join(str(x.value) for x in curr_tokens)}")
