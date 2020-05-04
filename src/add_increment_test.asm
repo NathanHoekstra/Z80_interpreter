0bae625665a47965b485dfd940112b9c9d103de9
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-05-04 19:41:16 +0200

    Added spaces to test asm file

--- src/add_increment_test.asm
+++ src/add_increment_test.asm
@@ -1,10 +1,13 @@
 ld a, #0    ;load 0 into register a
 ld b, a     ;load the value of register a into b
 ld b, $0F   ;load 15 into register b
+
 inc b       ;increment register b by 1
 inc b       ;increment register b by 1
+
 add a, #10  ;add 10 to register a
 add a, b    ;add register b to register a
+
 loop:
     inc b       ;increment register b by 1
     dec a       ;decrement a by one
